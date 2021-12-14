cf1827bd538897ac138b000ab47eebf14535be88
Moti Goldstein <motigoldstin@gmail.com>
2021-12-14 12:10:21 +0200

    Clean main.c
    Signed-off-by: Moti Goldstein <motigoldstin@gmail.com>

--- jce/c_cpp/homeEx4/main.c
+++ jce/c_cpp/homeEx4/main.c
@@ -2,12 +2,6 @@
 #include "StudentList.h"
 
 int main(){
-    int pt = 3;
-    int* ptr = &pt;
-    int* ptr2 = ptr;
-    printf("%d\n", ptr2 == ptr);
-    int p1 = 54;
-    ptr = &p1;
-    printf("%d\n", ptr2 == ptr);
+
     return 0;
 }
