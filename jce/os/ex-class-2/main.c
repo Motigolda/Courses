7b98022ff1f096a94831bfa80131f94be47cb038
Moti Goldstein <motigoldstin@gmail.com>
2022-02-23 11:36:45 +0200

    Add main program
    Signed-off-by: Moti Goldstein <motigoldstin@gmail.com>

---
+++ jce/os/ex-class-2/main.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+#include "word_count.h"
+
+int main(void){
+	char *str = malloc(1);	
+	
+	GetInputLineFromUser(&str);
+
+	if(str == NULL){
+		printf("Error: cant get input from user.\n");
+		exit(1);
+	}
+
+	int word_count = CountWords(str);
+
+	if (0 <= word_count)
+		printf("The number of words in the given string is %d.\n", word_count);
+	else
+		printf("Error occurred.\n");
+	
+	free(str);
+
+	return 0;
+}
