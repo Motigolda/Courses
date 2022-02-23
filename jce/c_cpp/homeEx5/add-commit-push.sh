5cdd578e9a9aab769c38e3a43a933736f7e3808a
Moti Goldstein <motigoldstin@gmail.com>
2022-02-23 11:13:08 +0200

    Some changes
    Signed-off-by: Moti Goldstein <motigoldstin@gmail.com>

---
+++ jce/c_cpp/homeEx5/add-commit-push.sh
@@ -0,0 +1,3 @@
+git add . 
+git commit -s -m "$1"
+git push origin home-ex-5
---
+++ jce/c_cpp/homeEx5/newCode.c
@@ -0,0 +1,8 @@
+#include <iostream>
+using namespace std;
+int main(){
+    int _rG0Q_7jgJ9_6 = 0;
+    _rG0Q_7jgJ9_6++;
+    yota;
+    return 0;
+}
