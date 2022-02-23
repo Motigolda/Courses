a2643726376dabb29e17a35c0022739cd2aa331c
Moti Goldstein <motigoldstin@gmail.com>
2022-02-23 11:29:23 +0200

    Basic Files
    Signed-off-by: Moti Goldstein <motigoldstin@gmail.com>

---
+++ jce/os/ex-class-2/word_count.h
@@ -0,0 +1,10 @@
+#ifndef WORD_COUNT_H
+#define WORD_COUNT_H
+// does nothing if line == NULL
+// assumes line is dynamic allocated memory
+void GetInputLineFromUser(char *line);
+
+// return -1 if failed
+int CountWords(char *line);
+
+#endif
