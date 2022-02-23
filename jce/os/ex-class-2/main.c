ff50bf464ffdd497cc529e339e5e633e5467a67e
Moti Goldstein <motigoldstin@gmail.com>
2022-02-23 13:27:41 +0200

    Add ExitIfNull() function to main.c
    Signed-off-by: Moti Goldstein <motigoldstin@gmail.com>

--- jce/os/ex-class-2/main.c
+++ jce/os/ex-class-2/main.c
@@ -3,13 +3,17 @@
 
 #include "word_count.h"
 
+static void ExitIfNull(void* p, char* msg);
+
 int main(void){
 
 	char *line =  NULL;	
 	line = GetInputLineFromUser();
 
-	int word_count = CountWords(str);
+	ExitIfNull(line, "Can\'t get input from user.");
 
+	int word_count = CountWords(line);
+	
 	if (0 <= word_count)
 		printf("The number of words in the given string is: %d.\n", word_count);
 	else
@@ -18,4 +22,14 @@ int main(void){
 	free(line);
 
 	return 0;
+}
+
+static void ExitIfNull(void* p, char* msg){
+	if (p != NULL)
+		return;
+
+	if (msg != NULL)
+		printf("Null Pointer Error: %s\n", msg);
+
+	exit(1);
 }
