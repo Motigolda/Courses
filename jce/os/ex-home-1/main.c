169c34fc72fe9c20e88728d093f9d1a3d10cff0a
Moti Goldstein <motigoldstin@gmail.com>
2022-03-03 13:06:52 +0200

    Remove old code
    Signed-off-by: Moti Goldstein <motigoldstin@gmail.com>

--- jce/os/ex-home-1/main.c
+++ jce/os/ex-home-1/main.c
@@ -129,12 +129,13 @@ void AppendEntryToHistory(history_t *history, char *entry){
     if (entry == NULL)
         return;
 
-    size_t current_histry_string_len = strlen(history->history_string);
+    size_t current_history_string_len = strlen(history->history_string);
     size_t new_entry_len = strlen(entry); 
     
-    history->history_string = realloc(history->history_string, current_histry_string_len + new_entry_len + 1);
+    history->history_string = realloc(history->history_string, current_history_string_len + new_entry_len + 1);
     
-    if (history->history_string[current_histry_string_len - 1] != '\n')
+    if (current_history_string_len > 0 &&
+        history->history_string[current_history_string_len - 1] != '\n')
         strcat(history->history_string, "\n");
 
     strcat(history->history_string, entry);
@@ -294,102 +295,3 @@ int main(void){
     
     return 0;
 }
-/*
-int main(void){
-    char *history;
-
-    FILE *history_fp = NULL;
-    history_fp = fopen(HISTORY_FILE_PATH, "r");
-
-    if(history_fp != NULL){
-        size_t text_file_size = 0;
-
-        fseek(history_fp, 0L, SEEK_END);
-        text_file_size = ftell(history_fp);
-        fseek(history_fp, 0L, SEEK_SET);
-
-        if(text_file_size > 0){
-            history = malloc(text_file_size + 1);
-            memset(history, 0, text_file_size + 1);
-            char current_line[MAX_LINE_LENGTH] = { 0 };
-            while((fgets(current_line, MAX_LINE_LENGTH, history_fp)) != NULL)
-                strcat(history, current_line); 
-        }
-        fclose(history_fp);
-        history_fp = NULL;
-    }
-    else{
-        history = malloc(1);
-        *history = 0;
-    }
-
-    char str_input[MAX_LINE_LENGTH] = { 0 };
-    while(true){
-        printf("Enter String [\'exit\' to exit, \'history\' to print history]: ");
-        
-        fgets(str_input, MAX_LINE_LENGTH, stdin);
-        str_input[MAX_LINE_LENGTH - 1] = 0;
-        
-        
-        if(str_input[0] == 0) 
-            continue;
-
-        if(strcmp(str_input, "exit") == 0)
-            break;
-        else if(strcmp(str_input, "history") == 0){
-            char *history_copy = malloc(strlen(history) + 1);
-            strcpy(history_copy, history);
-
-            char *line;
-            line = strtok(history_copy, "\n");
-            int history_entry_num = 0;
-            while(line != NULL){
-                printf("%d: %s\n", history_entry_num++, line);
-                line = strtok(NULL, "\n");
-            }
-            free(history_copy);
-        }
-        else{
-            unsigned int word_count = 0;
-            unsigned int char_count = 0;
-            bool in_word = false;
-            int i = 0;
-            while(str_input[i] != 0 && str_input[i] != '\n'){
-                if(str_input[i] != ' ' && !in_word){
-                    in_word = true;
-                    word_count++;
-                }
-
-                if(str_input[i] == ' ')
-                    in_word = false;
-                else
-                    char_count++;
-
-                i++;
-            }
-
-            printf("%d words\n%d chars\n", word_count, char_count);
-
-            history = realloc(history, strlen(history) + strlen(str_input) + 2);
-            if(history[strlen(history) - 1] != '\n')
-                strcat(history, "\n");
-             
-            strcat(history, str_input);
-            strcat(history, "\n");
-        }
-        
-    }
-    
-    history_fp = fopen(HISTORY_FILE_PATH, "w");
-
-    if(history_fp != NULL){
-        size_t file_length = strlen(history);
-        fwrite(history, file_length, 1, history_fp);
-        fclose(history_fp);
-        history_fp = NULL;
-    }
-
-    free(history);
-    return 0;
-}
-*/
