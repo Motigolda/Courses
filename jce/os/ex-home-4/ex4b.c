9d030b90901e1142cdb759b65057c3f0b41f628d
Moti Goldstein <motigoldstin@gmail.com>
2022-07-07 18:12:44 +0300

    Formatter output
    Signed-off-by: Moti Goldstein <motigoldstin@gmail.com>

--- jce/os/ex-home-4/ex4b.c
+++ jce/os/ex-home-4/ex4b.c
@@ -1,49 +1,65 @@
 #include <stdio.h>
+
 #include <stdint.h>
+
 #include <stdlib.h>
+
 #include <stdbool.h>
+
 #include <string.h>
+
 #include <pthread.h>
+
 #include <math.h>
+
 #include <sys/types.h>
+
 #include <sys/ipc.h>
+
 #include <sys/shm.h>
+
 #include <sys/wait.h>
+
 #include <unistd.h>
 
 #pragma region Typedefs
-typedef enum _option_selector_t{
+typedef enum _option_selector_t {
     OPT_UNKNOWN = 0,
-    OPT_INVALID = 1,
-    OPT_CALC_POLYNOMIAL,
-    OPT_CLOSE,
-    OPT_EMPTY_LINE
-} option_selector_t;
+        OPT_INVALID = 1,
+        OPT_CALC_POLYNOMIAL,
+        OPT_CLOSE,
+        OPT_EMPTY_LINE
+}
+option_selector_t;
 
-typedef struct _term_t{
+typedef struct _term_t {
     int32_t coefficient;
     int32_t exponent;
-} term_t;
+}
+term_t;
 
-typedef struct _polynomial_t{
-    term_t **terms; // ends with pointer to NULL  
-} polynomial_t;
+typedef struct _polynomial_t {
+    term_t ** terms; // ends with pointer to NULL  
+}
+polynomial_t;
 
-typedef struct _task_t{
+typedef struct _task_t {
     option_selector_t selected_option;
     polynomial_t polynomial;
     int32_t solve_for;
-} task_t;
+}
+task_t;
 
-typedef enum _buffers_in_parse_t{
+typedef enum _buffers_in_parse_t {
     BUFF_COEF,
     BUFF_EXP,
     BUFF_SOLVE_FOR
-} buffers_in_parse_t;
+}
+buffers_in_parse_t;
 
 #pragma endregion
 
-#pragma region Globals 
+#pragma region Globals
 
 static bool in_main_process = true;
 
@@ -51,8 +67,8 @@ static bool in_main_process = true;
 
 #pragma region Macros
 
-#define INPUT_LINE_MAX_LENGTH (512)
-#define MAX_TERMS (256)
+#define INPUT_LINE_MAX_LENGTH(512)
+#define MAX_TERMS(256)
 #define SHARED_MEMORY_SIZE sizeof(int64_t)
 
 #define STR_MEM_KEY "/tmp"
@@ -62,107 +78,108 @@ static bool in_main_process = true;
 #define STR_VALUE_OF_CALC_FORMAT "P(%d) = %ld\n"
 #define STR_CLOSE "done\n"
 
-#define IS_DIGIT(ch) ('0' <= (ch) && (ch) <= '9')
-#define IS_PLUS(ch) ((ch) == '+')
-#define IS_MINUS(ch) ((ch) == '-')
-#define IS_COMMA(ch) ((ch) == ',')
-#define IS_SPACE(ch) ((ch) == ' ')
-#define IS_NEWLINE(ch) ((ch) == '\n')
-#define IS_CIRCUMFLEX(ch) ((ch) == '^')
-#define IS_X(ch) ((ch) == 'x' || (ch) == 'X')
+#define IS_DIGIT(ch)('0' <= (ch) && (ch) <= '9')
+#define IS_PLUS(ch)((ch) == '+')
+#define IS_MINUS(ch)((ch) == '-')
+#define IS_COMMA(ch)((ch) == ',')
+#define IS_SPACE(ch)((ch) == ' ')
+#define IS_NEWLINE(ch)((ch) == '\n')
+#define IS_CIRCUMFLEX(ch)((ch) == '^')
+#define IS_X(ch)((ch) == 'x' || (ch) == 'X')
 
-#define SET_INVALID_AND_BREAK(task) task->selected_option = OPT_UNKNOWN;break;
+#define SET_INVALID_AND_BREAK(task) task -> selected_option = OPT_UNKNOWN;
+break;
 // use these to describe variable as in/out
-#define IN 
-#define OUT 
+#define IN
+#define OUT
 
 #pragma endregion
 
 #pragma region Type Functions Headers
-void AddTermToPolynomial(polynomial_t *polynomial, term_t *term);
+void AddTermToPolynomial(polynomial_t * polynomial, term_t * term);
 
-void CleanPolynomial(polynomial_t *polynomial);
+void CleanPolynomial(polynomial_t * polynomial);
 
-void CleanTask(task_t *task);
+void CleanTask(task_t * task);
 
 #pragma endregion
 
 #pragma region Component Functions Headers
-void GetLineFromUser(OUT char *line_from_user);
+void GetLineFromUser(OUT char * line_from_user);
 
-task_t *ParseUserInput(char *line_from_user);
+task_t * ParseUserInput(char * line_from_user);
 
-int64_t CalculatePolynomial(polynomial_t *polynomial, int32_t solve_for_x);
+int64_t CalculatePolynomial(polynomial_t * polynomial, int32_t solve_for_x);
 
-int64_t CalculateTerm(term_t *term, int32_t solve_for);
+int64_t CalculateTerm(term_t * term, int32_t solve_for);
 
-void CalculatePolynomialAndPrint(polynomial_t *polynomial, int32_t solve_for_x);
+void CalculatePolynomialAndPrint(polynomial_t * polynomial, int32_t solve_for_x);
 
-#pragma endregion 
+#pragma endregion
 // Heres the functions who changes with the calucation method
 #pragma region Calculation Functions Headers
 
-int64_t CalculatePolynomialUsingProcessesAndSharedMemory(polynomial_t *polynomial, int32_t solve_for);
+int64_t CalculatePolynomialUsingProcessesAndSharedMemory(polynomial_t * polynomial, int32_t solve_for);
 
 #pragma endregion
 
 #pragma region Handling Functions Headers
 void MainLoop();
 
-void HandleTask(task_t *task);
+void HandleTask(task_t * task);
 #pragma endregion
 
-int main( void ){
+int main(void) {
     MainLoop();
     return 0;
 }
 
 #pragma region Type Functions Implementation
-void AddTermToPolynomial(polynomial_t *polynomial, term_t *term){
-    if (polynomial->terms == NULL){
-        polynomial->terms = malloc(sizeof(term));
-        polynomial->terms[0] = NULL;
+void AddTermToPolynomial(polynomial_t * polynomial, term_t * term) {
+    if (polynomial -> terms == NULL) {
+        polynomial -> terms = malloc(sizeof(term));
+        polynomial -> terms[0] = NULL;
     }
 
     uint32_t i = 0;
-    while(polynomial->terms[i] != NULL)
+    while (polynomial -> terms[i] != NULL)
         i++;
-    
-    polynomial->terms = realloc(polynomial->terms, (i+2)*sizeof(term));
-    polynomial->terms[i] = malloc(sizeof(term_t));
-    polynomial->terms[i]->coefficient = term->coefficient;
-    polynomial->terms[i]->exponent = term->exponent;
-    polynomial->terms[i+1] = NULL;
+
+    polynomial -> terms = realloc(polynomial -> terms, (i + 2) * sizeof(term));
+    polynomial -> terms[i] = malloc(sizeof(term_t));
+    polynomial -> terms[i] -> coefficient = term -> coefficient;
+    polynomial -> terms[i] -> exponent = term -> exponent;
+    polynomial -> terms[i + 1] = NULL;
 }
 
-void CleanPolynomial(polynomial_t *polynomial){
+void CleanPolynomial(polynomial_t * polynomial) {
     if (polynomial == NULL)
         return;
 
-    if (polynomial->terms == NULL)
+    if (polynomial -> terms == NULL)
         return;
 
     uint32_t i = 0;
-    while(polynomial->terms[i] != NULL){
-        free(polynomial->terms[i]);
-        polynomial->terms[i] = NULL;
+    while (polynomial -> terms[i] != NULL) {
+        free(polynomial -> terms[i]);
+        polynomial -> terms[i] = NULL;
         i++;
     }
-    
-    free(polynomial->terms);
-    polynomial->terms = NULL;
+
+    free(polynomial -> terms);
+    polynomial -> terms = NULL;
 }
 
-void CleanTask(task_t *task){
+void CleanTask(task_t * task) {
     if (task == NULL)
         return;
-    
-    CleanPolynomial(&task->polynomial);
+
+    CleanPolynomial( & task -> polynomial);
 }
 #pragma endregion
 
 #pragma region Component Functions Implementation
-void GetLineFromUser(OUT char *line_from_user){
+void GetLineFromUser(OUT char * line_from_user) {
     if (line_from_user == NULL)
         return;
 
@@ -175,139 +192,131 @@ void GetLineFromUser(OUT char *line_from_user){
     line_from_user[INPUT_LINE_MAX_LENGTH - 1] = 0;
 }
 
-task_t *ParseUserInput(char *line_from_user){
+task_t * ParseUserInput(char * line_from_user) {
     if (line_from_user == NULL)
         return NULL;
-    
-    task_t *task = malloc(sizeof(task_t));
-    
+
+    task_t * task = malloc(sizeof(task_t));
+
     memset(task, 0, sizeof(task_t));
 
-    task->selected_option = OPT_UNKNOWN;
+    task -> selected_option = OPT_UNKNOWN;
 
     if (strcmp(line_from_user, STR_CLOSE) == 0)
-        task->selected_option = OPT_CLOSE;
+        task -> selected_option = OPT_CLOSE;
 
     if (IS_NEWLINE(line_from_user[0]))
-        task->selected_option = OPT_EMPTY_LINE;
+        task -> selected_option = OPT_EMPTY_LINE;
 
-    if (task->selected_option != OPT_UNKNOWN)
+    if (task -> selected_option != OPT_UNKNOWN)
         return task;
 
-    task->selected_option = OPT_CALC_POLYNOMIAL;
+    task -> selected_option = OPT_CALC_POLYNOMIAL;
     uint32_t i = 0;
     uint32_t buff_ind = 0;
-    char buffer[INPUT_LINE_MAX_LENGTH] = { 0 };
+    char buffer[INPUT_LINE_MAX_LENGTH] = {
+        0
+    };
     bool is_buffering = true;
     buffers_in_parse_t current_buffer = BUFF_COEF;
     char current_char = line_from_user[0];
-    term_t current_term = { 0 };
+    term_t current_term = {
+        0
+    };
 
-    while(current_char != 0){
-        if (is_buffering){
-            if (IS_DIGIT(current_char)){
+    while (current_char != 0) {
+        if (is_buffering) {
+            if (IS_DIGIT(current_char)) {
                 buffer[buff_ind] = current_char;
                 ++buff_ind;
-            }
-            else if(IS_X(current_char)){
-                if(current_buffer == BUFF_COEF){
+            } else if (IS_X(current_char)) {
+                if (current_buffer == BUFF_COEF) {
                     current_term.coefficient = atoi(buffer);
                     memset(buffer, 0, INPUT_LINE_MAX_LENGTH);
                     buff_ind = 0;
                     current_buffer = BUFF_EXP;
                     is_buffering = false;
-                }
-                else{
+                } else {
                     SET_INVALID_AND_BREAK(task);
                 }
-                    
-            }
-            else if (IS_PLUS(current_char)){
-                if (current_buffer == BUFF_EXP){
+
+            } else if (IS_PLUS(current_char)) {
+                if (current_buffer == BUFF_EXP) {
                     current_term.exponent = atoi(buffer);
                     memset(buffer, 0, INPUT_LINE_MAX_LENGTH);
                     buff_ind = 0;
                     current_buffer = BUFF_COEF;
-                    AddTermToPolynomial(&task->polynomial, &current_term);
+                    AddTermToPolynomial( & task -> polynomial, & current_term);
                     current_term.coefficient = 0;
                     current_term.exponent = 0;
-                }
-                else {
+                } else {
                     SET_INVALID_AND_BREAK(task);
-                }   
-                    
-            }
-            else if(IS_COMMA(current_char)){
-                if (current_buffer == BUFF_EXP){
+                }
+
+            } else if (IS_COMMA(current_char)) {
+                if (current_buffer == BUFF_EXP) {
                     current_term.exponent = atoi(buffer);
                     memset(buffer, 0, INPUT_LINE_MAX_LENGTH);
                     buff_ind = 0;
-                    AddTermToPolynomial(&task->polynomial, &current_term);
+                    AddTermToPolynomial( & task -> polynomial, & current_term);
                     current_term.coefficient = 0;
                     current_term.exponent = 0;
                     is_buffering = false;
-                }
-                else{
+                } else {
                     SET_INVALID_AND_BREAK(task);
                 }
-                    
-            }
-            else if(IS_NEWLINE(current_char)){
-                if(current_buffer == BUFF_SOLVE_FOR){
-                    task->solve_for = atoi(buffer);
+
+            } else if (IS_NEWLINE(current_char)) {
+                if (current_buffer == BUFF_SOLVE_FOR) {
+                    task -> solve_for = atoi(buffer);
                     break;
-                }
-                else{
+                } else {
                     SET_INVALID_AND_BREAK(task);
                 }
+            } else if (IS_CIRCUMFLEX(current_char)) {
+                SET_INVALID_AND_BREAK(task);
             }
-            else if(IS_CIRCUMFLEX(current_char)){
+        } else {
+            if (IS_SPACE(current_char)) {
+                is_buffering = true;
+                current_buffer = BUFF_SOLVE_FOR;
+            } else if (IS_CIRCUMFLEX(current_char)) {
+                is_buffering = true;
+            } else {
                 SET_INVALID_AND_BREAK(task);
             }
         }
-        else{
-            if(IS_SPACE(current_char)){
-                is_buffering = true;
-                current_buffer = BUFF_SOLVE_FOR;
-            }
-            else if(IS_CIRCUMFLEX(current_char)){
-                is_buffering = true;
-            }
-            else{
-                SET_INVALID_AND_BREAK(task);
-            }
-        }     
 
         i++;
         current_char = line_from_user[i];
     }
 
-    if(task->selected_option == OPT_INVALID){
-        CleanPolynomial(&task->polynomial);
+    if (task -> selected_option == OPT_INVALID) {
+        CleanPolynomial( & task -> polynomial);
     }
     return task;
 }
 
-int64_t CalculatePolynomial(polynomial_t *polynomial, int32_t solve_for){
+int64_t CalculatePolynomial(polynomial_t * polynomial, int32_t solve_for) {
     if (polynomial == NULL)
         return 0;
-    
+
     return CalculatePolynomialUsingProcessesAndSharedMemory(polynomial, solve_for);
 }
 
-int64_t CalculateTerm(term_t *term, int32_t solve_for){
+int64_t CalculateTerm(term_t * term, int32_t solve_for) {
     if (term == NULL)
         return 0;
 
-    double coef = (double)term->coefficient;
-    double exp = (double)term->exponent;
+    double coef = (double) term -> coefficient;
+    double exp = (double) term -> exponent;
 
     int64_t result = (int64_t)(pow(solve_for, exp) * coef);
     return result;
 }
 
-void CalculatePolynomialAndPrint(polynomial_t *polynomial, int32_t solve_for_x){
-    if(polynomial == NULL)
+void CalculatePolynomialAndPrint(polynomial_t * polynomial, int32_t solve_for_x) {
+    if (polynomial == NULL)
         return 0;
 
     int64_t result = CalculatePolynomial(polynomial, solve_for_x);
@@ -315,19 +324,19 @@ void CalculatePolynomialAndPrint(polynomial_t *polynomial, int32_t solve_for_x){
     if (in_main_process)
         printf(STR_VALUE_OF_CALC_FORMAT, solve_for_x, result);
 }
-#pragma endregion 
+#pragma endregion
 
 #pragma region Calculation Functions Implementations
 
-int64_t CalculatePolynomialUsingProcessesAndSharedMemory(polynomial_t *polynomial, int32_t solve_for){
-    if(polynomial == NULL)
+int64_t CalculatePolynomialUsingProcessesAndSharedMemory(polynomial_t * polynomial, int32_t solve_for) {
+    if (polynomial == NULL)
         return 0;
-    
+
     key_t key;
     int err = 0;
     err = key = ftok(STR_MEM_KEY, 'y');
 
-    if(err == -1){
+    if (err == -1) {
         perror("ftok failed");
         exit(EXIT_FAILURE);
     }
@@ -337,42 +346,40 @@ int64_t CalculatePolynomialUsingProcessesAndSharedMemory(polynomial_t *polynomia
         key, SHARED_MEMORY_SIZE, IPC_CREAT | IPC_EXCL | 0600
     );
 
-    if (err == -1){
+    if (err == -1) {
         perror("shmget failed");
         exit(EXIT_FAILURE);
     }
 
-    int64_t *shm_ptr; 
-    shm_ptr = (int64_t*)shmat(shm_id, NULL, 0);
+    int64_t * shm_ptr;
+    shm_ptr = (int64_t * ) shmat(shm_id, NULL, 0);
 
-    term_t *current_term = polynomial->terms[0];
+    term_t * current_term = polynomial -> terms[0];
     uint32_t term_counter = 0;
     pid_t current_pid;
-    while(current_term != NULL){
+    while (current_term != NULL) {
         current_pid = fork();
-        if (current_pid < 0){
+        if (current_pid < 0) {
             perror("fork() failed");
             exit(EXIT_FAILURE);
-        }
-        else if(current_pid == 0){
-            *shm_ptr += CalculateTerm(current_term, solve_for);
+        } else if (current_pid == 0) {
+            * shm_ptr += CalculateTerm(current_term, solve_for);
             current_term = 0;
-            in_main_process = false;         
-        }
-        else{
+            in_main_process = false;
+        } else {
             ++term_counter;
-            current_term = polynomial->terms[term_counter];
+            current_term = polynomial -> terms[term_counter];
         }
     }
 
-    for(int i = 0; i < term_counter; i++)
+    for (int i = 0; i < term_counter; i++)
         wait(NULL);
-    int64_t result = *shm_ptr;
+    int64_t result = * shm_ptr;
 
-    if(current_pid != 0){
+    if (current_pid != 0) {
         shmdt(shm_ptr);
         err = shmctl(shm_id, IPC_RMID, NULL);
-        if (err == -1){
+        if (err == -1) {
             perror("shmctl failed");
             exit(EXIT_FAILURE);
         }
@@ -384,11 +391,13 @@ int64_t CalculatePolynomialUsingProcessesAndSharedMemory(polynomial_t *polynomia
 #pragma endregion
 
 #pragma region Handling Functions Implementations
-void MainLoop(){
-    char line_buffer[INPUT_LINE_MAX_LENGTH] = { 0 };
+void MainLoop() {
+    char line_buffer[INPUT_LINE_MAX_LENGTH] = {
+        0
+    };
     bool keep_running = true;
-    task_t *task = NULL;
-    while(keep_running && in_main_process){
+    task_t * task = NULL;
+    while (keep_running && in_main_process) {
         memset(line_buffer, 0, INPUT_LINE_MAX_LENGTH);
 
         GetLineFromUser(line_buffer);
@@ -396,8 +405,8 @@ void MainLoop(){
         task = ParseUserInput(line_buffer);
 
         HandleTask(task);
-        
-        if(task->selected_option == OPT_CLOSE)
+
+        if (task -> selected_option == OPT_CLOSE)
             keep_running = false;
 
         CleanTask(task);
@@ -406,14 +415,13 @@ void MainLoop(){
     }
 }
 
-void HandleTask(task_t *task){
+void HandleTask(task_t * task) {
     if (task == NULL)
         return;
 
-    switch (task->selected_option)
-    {
+    switch (task -> selected_option) {
     case OPT_CALC_POLYNOMIAL:
-        CalculatePolynomialAndPrint(&task->polynomial, task->solve_for);
+        CalculatePolynomialAndPrint( & task -> polynomial, task -> solve_for);
         break;
 
     case OPT_EMPTY_LINE:
@@ -421,7 +429,7 @@ void HandleTask(task_t *task){
 
     case OPT_CLOSE:
         break;
-    
+
     case OPT_INVALID:
         printf("%s\n", STR_INVALID_INPUT);
         break;
